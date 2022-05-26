a502bd671744c89f4173857d06d66cf68ddfc230
markov vadim <markovadim@gmail.com>
2022-05-26 22:12:29 +0300

    master: created empty project

---
+++ resume-api-spring-boot/src/main/java/by/markov/resumeapispringboot/ResumeApiSpringBootApplication.java
@@ -0,0 +1,13 @@
+package by.markov.resumeapispringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeApiSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeApiSpringBootApplication.class, args);
+	}
+
+}
---
+++ resume-api-spring-boot/src/test/java/by/markov/resumeapispringboot/ResumeApiSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package by.markov.resumeapispringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeApiSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
