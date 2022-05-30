caf43dcb7a89648fae20bfe51e28a2d605a7e30e
markov vadim <markovadim@gmail.com>
2022-05-30 23:47:42 +0300

    master: added find resume by id method

--- resume-api-spring-boot/src/main/java/by/markov/resumeapispringboot/controller/ResumeController.java
+++ resume-api-spring-boot/src/main/java/by/markov/resumeapispringboot/controller/ResumeController.java
@@ -5,7 +5,10 @@ import by.markov.resumeapispringboot.entity.Resume;
 import by.markov.resumeapispringboot.repository.ResumeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/resume")
@@ -22,5 +25,18 @@ public class ResumeController {
         } catch (Exception e) {
             return ResponseEntity.badRequest().body("Error");
         }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity printResumeById(@PathVariable Integer id) {
+        try {
+            Optional<Resume> resume = resumeRepository.findById(id);
+            if (!resume.isEmpty()) {
+                return ResponseEntity.ok(resume);
+            }
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("User not found with id:" + id);
+        }
+        return ResponseEntity.ok("User with id:" + id + " was found");
     }
 }
