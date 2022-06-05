c865a633bcad08c21c4a8fc3cf09f2a324550176
markov vadim <markovadim@gmail.com>
2022-06-05 23:52:35 +0300

    master: added logging of controller

--- resume-api-spring-boot/src/main/java/by/markov/resumeapispringboot/controller/ResumeController.java
+++ resume-api-spring-boot/src/main/java/by/markov/resumeapispringboot/controller/ResumeController.java
@@ -5,6 +5,8 @@ import by.markov.resumeapispringboot.exceptions.ResumeAlreadyExistException;
 import by.markov.resumeapispringboot.exceptions.ResumeNotFoundException;
 import by.markov.resumeapispringboot.service.ResumeServiceImpl;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,6 +16,8 @@ import org.springframework.web.bind.annotation.*;
 public class ResumeController {
 
     private final ResumeServiceImpl resumeService;
+
+    private static final Logger logger = LoggerFactory.getLogger(ResumeController.class);
 
     @GetMapping
     public ResponseEntity showAllResumes() {
@@ -37,46 +41,63 @@ public class ResumeController {
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity deleteResumeById(@PathVariable Integer id) throws ResumeNotFoundException {
+    public Resume deleteResumeById(@PathVariable Integer id) throws ResumeNotFoundException {
+        Resume resume = null;
         try {
-            return ResponseEntity.ok(resumeService.deleteResume(id));
+            resume = resumeService.deleteResume(id);
+            logger.info("DELETE method of resume with id:" + id);
         } catch (ResumeNotFoundException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
+            logger.error("Resume with id:" + id + " not found.");
+            throw new ResumeNotFoundException();
         } catch (Exception e) {
-            return ResponseEntity.badRequest().body("Error");
+            e.printStackTrace();
         }
+        return resume;
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity updateResume(@PathVariable Integer id, @RequestBody Resume resume) {
+    public Resume updateResume(@PathVariable Integer id, @RequestBody Resume newResume) throws ResumeNotFoundException {
+        Resume resume = null;
         try {
-            return ResponseEntity.ok(resumeService.updateResume(id, resume));
+            resume = resumeService.updateResume(id, newResume);
+            logger.info("PUT method by URL /resume/" + id + " with parameter new resume: " + newResume);
         } catch (ResumeNotFoundException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
+            logger.error("Resume with id:" + id + " not found.");
+            throw new ResumeNotFoundException();
         } catch (Exception e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
+            e.printStackTrace();
         }
+        return resume;
     }
 
     @GetMapping("/id")
-    public ResponseEntity findResumeById(@RequestParam Integer id) {
+    public Resume findResumeById(@RequestParam Integer id) throws ResumeNotFoundException {
+        Resume resume = null;
         try {
-            return ResponseEntity.ok(resumeService.findResumeById(id));
+            resume = resumeService.findResumeById(id);
+            logger.info("GET method with URL /resume/id (id:" + id + ")\nResponse: " + resume);
+            return resume;
         } catch (ResumeNotFoundException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
+            logger.error("Resume not found with id:" + id);
+            throw new ResumeNotFoundException();
         } catch (Exception e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
+            e.printStackTrace();
         }
+        return resume;
     }
 
     @GetMapping("/user")
-    public ResponseEntity findResumeByUser(@RequestParam String user) {
+    public Resume findResumeByUser(@RequestParam String user) throws ResumeNotFoundException {
+        Resume resume = null;
         try {
-            return ResponseEntity.ok(resumeService.findResumeByUser(user));
+            resume = resumeService.findResumeByUser(user);
+            logger.info("GET method with URL /resume/user (user:" + user + ")\nResponse: " + resume);
         } catch (ResumeNotFoundException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
+            logger.error("Resume not found with user:" + user);
+            throw new ResumeNotFoundException();
         } catch (Exception e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
+            e.printStackTrace();
         }
+        return resume;
     }
 }
